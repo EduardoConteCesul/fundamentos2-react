678dac6ff0dfd8aef54692ef92239dc637d7a5cf
EduardoConteCesul <eduardo.2024014337@alunocesul.com>
2025-10-06 22:43:16 -0300

    styled-components com atributos

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
+import { Loadind } from "@components/Loading";
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
 import { Teams } from "@screens/Teams";
 import theme from "@theme/index";
 import { ThemeProvider } from "styled-components/native";
 
 export default function App() {
+  const [fontsLoadead] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
-      <Teams />
+      {!fontsLoadead ? <Teams /> : <Loadind />}
     </ThemeProvider>
   );
 }
--- src/screens/Teams/index.tsx
+++ src/screens/Teams/index.tsx
@@ -1,13 +1,11 @@
 import logoImg from "@assets/Logo.png";
 import { Image, Text } from "react-native";
 import * as S from "./styles";
-import { StatusBar } from "expo-status-bar";
 
 export function Teams() {
   return (
     <S.Container>
       <Text>Groups</Text>
-      <StatusBar style="auto"/>
       <Image source={logoImg} />
     </S.Container>
   )
